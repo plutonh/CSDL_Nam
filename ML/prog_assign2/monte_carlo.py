59d0b9c4e311efabcc76cede74e93c1858d016e9
plutonh <jonghyen51@gmail.com>
2024-02-12 22:55:20 +0900

    2024_02_12

--- ML/prog_assign2/monte_carlo.py
+++ ML/prog_assign2/monte_carlo.py
@@ -27,6 +27,29 @@ def off_policy_mc_prediction_ordinary_importance_sampling(
     # TODO: Implement Off Policy Monte-Carlo prediction algorithm using ordinary importance
     # sampling (Hint: Sutton Book p. 109, every-visit implementation is fine)
     #####################
+    
+    state_len = env_spec._nS
+    action_len = env_spec._nA
+    gamma = env_spec.gamma
+    
+    # Initiallize
+    Q = initQ
+    C = np.zeros((state_len, action_len)) # n
+    
+    # Loop
+    for episode in trajs:
+        G = 0
+        W = 1
+        
+        for step in range(len(episode) - 1, -1, -1): # backtrace?
+            s = episode[step][0] # s_t
+            a = episode[step][1] # a_t
+            r = episode[step][2] #r_{t+1}
+            
+            G = gamma * G + r
+            C[s][a] += 1
+            Q[s][a] = Q[s][a] + ((W * G) - Q[s][a]) / C[s][a]
+            W = W * (pi.action_prob[s][a] / bpi.action_prob[s][a])
 
     return Q
 
@@ -53,5 +76,28 @@ def off_policy_mc_prediction_weighted_importance_sampling(
     # TODO: Implement Off Policy Monte-Carlo prediction algorithm using weighted importance
     # sampling (Hint: Sutton Book p. 110, every-visit implementation is fine)
     #####################
+    
+    state_len = env_spec._nS
+    action_len = env_spec._nA
+    gamma = env_spec.gamma
+    
+    # Initiallize
+    Q = initQ
+    C = np.zeros((state_len, action_len))
+    
+    # Loop
+    for episode in trajs:
+        G = 0
+        W = 1
+        
+        for step in range(len(episode) - 1, -1, -1): # backtrace?
+            s = episode[step][0] # s_t
+            a = episode[step][1] # a_t
+            r = episode[step][2] #r_{t+1}
+            
+            G = gamma * G + r
+            C[s][a] += W
+            Q[s][a] = Q[s][a] + (W / C[s][a]) * (G - Q[s][a])
+            W = W * (pi.action_prob[s][a] / bpi.action_prob[s][a])
 
     return Q
